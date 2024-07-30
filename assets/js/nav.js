1ac2899d3e3e848e7a5423d1fc457c0abd9a0ba2
thomas hunckler <hunckler.thomas@hotmail.fr>
2024-07-30 19:03:03 +0200

    change css

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,1 +1,9 @@
-let navMain = document.getElementById("#nav_main");
+//let navMain = document.getElementById("#nav_main");
+console.log("red");
+let burgers = Array.from(document.querySelectorAll(".burger"));
+burgers.forEach((element) =>{
+    element.addEventListener("click", () => {
+        console.log("blue");
+        document.querySelector(".mobile_nav").classList.toggle("visible");
+    });
+})
